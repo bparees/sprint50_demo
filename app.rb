2908cc24f8cad430cca91761d72b1cb65a7b9387
Ben Parees <bparees@redhat.com>
2014-09-11 14:00:20 -0400

    restructure

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+
+set :bind, '0.0.0.0'
+set :port,8080
+get '/' do
+  "Hello World!<br>
+  User is #{ENV['ADMIN_USERNAME']}<br>
+  Password is #{ENV['ADMIN_PASSWORD']}<br>
+  DB password is #{ENV['DB_PASSWORD']}"
+
+end
