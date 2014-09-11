fe3a7b5f4cd87983a8cf8027d89a981f59f5bd8f
Ben Parees <bparees@redhat.com>
2014-09-11 17:36:03 -0400

    update

--- app/app.rb
+++ app/app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port,8080
 get '/' do
-  "Hello World2!<br>
+  "Hello World3!<br>
   User is #{ENV['ADMIN_USERNAME']}<br>
   Password is #{ENV['ADMIN_PASSWORD']}<br>
   DB password is #{ENV['DB_PASSWORD']}"
