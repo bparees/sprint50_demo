69ef9095598a3ccc125d5c5cfaeea54e993f61ec
Ben Parees <bparees@redhat.com>
2014-09-11 21:28:10 -0400

    .

--- app/app.rb
+++ app/app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port,8080
 get '/' do
-  "Hello World2!
+  "Hello World!
   User is #{ENV['ADMIN_USERNAME']}
   Password is #{ENV['ADMIN_PASSWORD']}
   DB password is #{ENV['DB_PASSWORD']}\n"
